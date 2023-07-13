10f8e74cb63f1e79d27a12e907dc1074cdbb2eed
Samson Tush <samo4421@icloud.com>
2023-07-13 20:50:39 +0300

    Add delete, edit, create, update

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,13 +1,12 @@
 class BlogPostsController < ApplicationController
+    before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @blog_posts = BlogPost.all
     end
 
     def show
-        @blog_post = BlogPost.find(params[:id])
-        
-      rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
+    
     end
 
     def new
@@ -23,9 +22,31 @@ class BlogPostsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @blog_post.update(blog_post_params)
+            redirect_to @blog_post
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @blog_post.destroy
+        redirect_to root_path
+    end
+
     private
 
     def blog_post_params
         params.require(:blog_post).permit(:title, :img, :body)
+    end
+
+    def set_blog_post
+        @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
-  get '/blog_posts/:id', to:  'blog_posts#show', as: :blog_post
-  post '/blog_posts', to: 'blog_posts#create', as: :blog_posts
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to:  "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#destroy"
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  resources :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
