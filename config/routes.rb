f623b41dfc196b14e7cce4244f8c50e11c15172e
Samson Tush <samo4421@icloud.com>
2023-07-13 15:47:01 +0300

    Add link_to and show in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/blog_posts/:id', to:  'blog_posts#show'
+
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
